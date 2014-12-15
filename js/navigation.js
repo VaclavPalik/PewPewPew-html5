436fa43a8d6c8eae95a1a4e02e496ae44fcbdf0c
Vaclav Palik <vaclavpalik@gmail.com>
2014-12-15 20:09:40 +0100

    navigation js

---
+++ js/navigation.js
@@ -0,0 +1,39 @@
+/* jshint devel: true */
+!function() {
+	'use strict';
+
+	var navGame = document.getElementById("nav-game");
+	var navUpgrade = document.getElementById("nav-upgrade");
+	var navAbout = document.getElementById("nav-about");
+
+	var showElement = function(element) {
+		document.getElementById(element).className = document
+				.getElementById(element).className.replace(
+				/(?:^|\s)invisible(?!\S)/g, '');
+	};
+	var hideElement = function(element) {
+		document.getElementById(element).className += "invisible";
+	};
+	
+	var showGame = function() {
+		showElement("game");
+		hideElement("upgrades");
+		hideElement("about");
+	};
+	
+	var showUpgrades = function() {
+		hideElement("game");
+		showElement("upgrades");
+		hideElement("about");
+	};
+	
+	var showAbout = function() {
+		hideElement("game");
+		hideElement("upgrades");
+		showElement("about");
+	};
+	
+	navGame.addEventListener("click", showGame);
+	navUpgrade.addEventListener("click", showUpgrades);
+	navAbout.addEventListener("click", showAbout);
+}();
